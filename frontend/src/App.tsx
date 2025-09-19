c8eb2468e125577af4416b26aafb431f4bc5c352
Christian Sanches <christian.sanches-v@uplevelteam.com>
2025-09-18 21:28:54 -0300

    setting up frontend with vite and integrated graphql with frontned

---
+++ frontend/src/App.tsx
@@ -0,0 +1,47 @@
+import { gql } from "@apollo/client";
+import { useQuery } from "@apollo/client/react";
+
+const LIST_CHARACTERS = gql`
+  query {
+    listCharacters {
+      id
+      name
+      job
+    }
+  }
+`;
+
+type Character = {
+  id: string;
+  name: string;
+  job: string;
+};
+
+type ListCharactersResponse = {
+  listCharacters: Character[];
+};
+
+function App() {
+  const { loading, error, data } =
+    useQuery<ListCharactersResponse>(LIST_CHARACTERS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  return (
+    <>
+      <div>
+        <h1>Characters</h1>
+        <ul>
+          {data?.listCharacters.map((char: Character) => (
+            <li key={char.id}>
+              {char.name} ({char.job})
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default App;
